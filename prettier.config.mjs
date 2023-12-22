518393a5acdd8f4a89ed164a4de3a1aca3794b43
spicyzboss <supachai@spicyz.io>
2023-12-22 22:48:14 +0700

    style: code formats before commit

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: Infinity,
+  semi: true,
+  useTabs: false,
+  singleQuote: true,
+  tabWidth: 2,
+  tabs: false,
+  quoteProps: "preserve",
+  bracketSpacing: true
+}
